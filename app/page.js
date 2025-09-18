99ba91a73aac6802da3c9784e5ae8dee8a478715
KT147 <kevin.tammm@gmail.com>
2025-09-18 13:39:35 +0300

    <Suspense> ootamine

--- app/page.js
+++ app/page.js
@@ -1,12 +1,18 @@
+import fs from "node:fs/promises";
 import ClientDemo from "./components/ClientDemo";
 import DataFetchingDemo from "./components/DataFetchingDemo";
 import RSCDemo from "./components/RSCDemo";
 import ServerActionsDemo from "./components/ServerActionsDemo";
+import UsePromiseDemo from "./components/UsePromisesDemo";
+import { Suspense } from "react";
 
-export default function Home() {
+export default async function Home() {
   return (
     <main>
-      <DataFetchingDemo />
+      {/* <DataFetchingDemo /> */}
+      <Suspense>
+        <UsePromiseDemo />
+      </Suspense>
       <ServerActionsDemo />
     </main>
   );
