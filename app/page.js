d92180b21df61059a5b03cd954d55c79a58d1bfa
KT147 <kevin.tammm@gmail.com>
2025-09-18 13:32:52 +0300

    sending data

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 import ClientDemo from "./components/ClientDemo";
 import DataFetchingDemo from "./components/DataFetchingDemo";
 import RSCDemo from "./components/RSCDemo";
+import ServerActionsDemo from "./components/ServerActionsDemo";
 
 export default function Home() {
   return (
     <main>
       <DataFetchingDemo />
+      <ServerActionsDemo />
     </main>
   );
 }
