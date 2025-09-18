be008423425349a281b4ef05a24e175664c2b56b
KT147 <kevin.tammm@gmail.com>
2025-09-18 13:50:19 +0300

    use() kasutamine

--- app/page.js
+++ app/page.js
@@ -7,11 +7,19 @@ import UsePromiseDemo from "./components/UsePromisesDemo";
 import { Suspense } from "react";
 
 export default async function Home() {
+  const fetchUsersPromise = new Promise((resolve) =>
+    setTimeout(async () => {
+      const data = await fs.readFile("dummy-db.json", "utf-8");
+      const users = JSON.parse(data);
+      resolve(users);
+    }, 2000)
+  );
+
   return (
     <main>
       {/* <DataFetchingDemo /> */}
-      <Suspense>
-        <UsePromiseDemo />
+      <Suspense fallback={<p>Loading users...</p>}>
+        <UsePromiseDemo usersPromise={fetchUsersPromise} />
       </Suspense>
       <ServerActionsDemo />
     </main>
